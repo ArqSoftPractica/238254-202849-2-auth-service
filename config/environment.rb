2895e83f2199fd523835aa7b34d766be6ffb6eac
AgustinH09 <agus.hernandez9@gmail.com>
2023-05-14 18:06:47 -0300

    [project-intialization]: fixes

--- config/environment.rb
+++ config/environment.rb
@@ -3,10 +3,4 @@ require 'bundler/setup'
 
 Bundler.require(:default, ENV.fetch('RACK_ENV', 'development'))
 
-default :autoload_paths, %w[
-  controllers
-  models
-  lib/middlewares
-]
-
 require_all 'app'
